496ce76f1ed907226ea8f9bf604dfdd525dd54b9
Cibermatico <149595043+Cibermatico@users.noreply.github.com>
2024-03-11 21:30:33 +0100

    normato

--- init_utils.c
+++ init_utils.c
@@ -12,7 +12,7 @@
 
 #include "utils.h"
 
-void fractal_init(parameters *params)
+void	fractal_init(parameters *params)
 {
 	params->xshift = 0;
 	params->yshift = 0;
@@ -20,29 +20,32 @@ void fractal_init(parameters *params)
 	params->iterations = 40;
 	params->z0 = 0;
 	params->j_re = 0.355534;
-	params->j_im = -0.337292;  
-	params -> pc_re = 0.5666 ; //manowar -0.01
-	params -> pc_im = 0.0;  
-	params -> pk_re = -0.5;    //manowar 1
+	params->j_im = -0.337292;
+	params -> pc_re = 0.5666 ;
+	params -> pc_im = 0.0;
+	params -> pk_re = -0.5;
 	params -> pk_im = 0.0;
 }
 
-void system_init(app_data *app)
+void	system_init(app_data *app)
 {
 	app->mlx_data.mlx = mlx_init();
-	app->mlx_data.mlx_win = mlx_new_window(app->mlx_data.mlx, 800, 600, "Fractal explorer");
+	app->mlx_data.mlx_win = mlx_new_window(app->mlx_data.mlx, 800, 600, \
+			"Fractal explorer");
 	app->mlx_data.img = mlx_new_image(app->mlx_data.mlx, 800, 600);
-	app->mlx_data.addr = mlx_get_data_addr(app->mlx_data.img, &app->mlx_data.bits_per_pixel, &app->mlx_data.line_length, &app->mlx_data.endian);   
-	mlx_key_hook(app->mlx_data.mlx_win, &key_hook, app); 
+	app->mlx_data.addr = mlx_get_data_addr(app->mlx_data.img, \
+			&app->mlx_data.bits_per_pixel, &app->mlx_data.line_length, \
+			&app->mlx_data.endian);
+	mlx_key_hook(app->mlx_data.mlx_win, &key_hook, app);
 	mlx_mouse_hook(app->mlx_data.mlx_win, &mouse_scroll_hook, app);
-	mlx_hook(app->mlx_data.mlx_win, 17, 1L<<0, &close_button_hook, &(app->mlx_data));
+	mlx_hook(app->mlx_data.mlx_win, 17, 1L << 0, &close_button_hook, \
+			&(app->mlx_data));
 }
 
-void ship_init(int *i, Complex *z, parameters *params)
+void	ship_init(int *i, Complex *z, parameters *params)
 {
 	*i = 0;
 	z->re = params-> z0;
-	z->im = -params-> z0;    
-	return;
+	z->im = -params-> z0;
+	return ;
 }
-
