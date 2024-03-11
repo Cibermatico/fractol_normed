3db8a6c5c14b114979a8d3ce60a0b5f74724270d
Cibermatico <149595043+Cibermatico@users.noreply.github.com>
2024-03-11 13:16:00 +0100

    normato

--- color_utils.c
+++ color_utils.c
@@ -14,40 +14,39 @@
 #include <printf.h>
 #include "utils.h"
 
-
 /*channel color = sin(frequency * continuous_index + phase) * center + delta)*/
 
-color_render m_color(int i)
+color_render	m_color(int i)
 {
-	return (color_render){(unsigned int)(sin(0.071 * i + 1) * 127.5 + 127.5),
-						  (unsigned int)(sin(0.071 * i + 2) * 127.5 + 127.5),
-						  (unsigned int)(sin(0.071 * i + 3) * 12.5 + 127.5)};
+	return ((color_render){(unsigned int)(sin(0.071 * i + 1) * 127.5 + 127.5),
+						(unsigned int)(sin(0.071 * i + 2) * 127.5 + 127.5),
+						(unsigned int)(sin(0.071 * i + 3) * 12.5 + 127.5)});
 }
 
-color_render j_color(int i)
+color_render	j_color(int i)
 {
-	return (color_render){(unsigned int)(sin(0.093 * i + 0.5) * 127.5 + 127.5),
-						  (unsigned int)(cos(0.033 * i + 2) * 127.5 + 127.5),
-						  (unsigned int)(cos(0.0623 * i + 1) * 117.5 + 127.5)};
+	return ((color_render){(unsigned int)(sin(0.093 * i + 0.5) * 127.5 + 127.5),
+						(unsigned int)(cos(0.033 * i + 2) * 127.5 + 127.5),
+						(unsigned int)(cos(0.0623 * i + 1) * 117.5 + 127.5)});
 }
 
-color_render s_color(int i)
+color_render	s_color(int i)
 {
-	return (color_render){(unsigned int)(sin(0.071 * i + 1) * 127.5 + 127.5),
-						  (unsigned int)(sin(0.071 * i + 2) * 127.5 + 127.5),
-						  (unsigned int)(sin(0.171 * i + 3) * 127.5 + 127.5)};
+	return ((color_render){(unsigned int)(sin(0.071 * i + 1) * 127.5 + 127.5),
+						(unsigned int)(sin(0.071 * i + 2) * 127.5 + 127.5),
+						(unsigned int)(sin(0.171 * i + 3) * 127.5 + 127.5)});
 }
 
-color_render p_color(int i)
+color_render	p_color(int i)
 {
-	return (color_render){(unsigned int)(sin(0.095 * i + 1) * 127.5 + 127.5),
-						  (unsigned int)(sin(0.087 * i + 2.4) * 127.5 + 127.5),
-						  (unsigned int)(sin(0.178 * i + 3.6) * 127.5 + 127.5)};
+	return ((color_render){(unsigned int)(sin(0.095 * i + 1) * 127.5 + 127.5),
+						(unsigned int)(sin(0.087 * i + 2.4) * 127.5 + 127.5),
+						(unsigned int)(sin(0.178 * i + 3.6) * 127.5 + 127.5)});
 }
 
-int color_blender(int i, parameters *params)
+int	color_blender(int i, parameters *params)
 {
-	color_render color;
+	color_render	color;
 
 	if (params->type == 'm')
 		color = m_color(i);
@@ -57,10 +56,5 @@ int color_blender(int i, parameters *params)
 		color = s_color(i);
 	else if (params->type == 'p')
 		color = p_color(i);
-	return get_hexcolor(color.r, color.g, color.b);
+	return (get_hexcolor(color.r, color.g, color.b));
 }
-
-
-
-
-
